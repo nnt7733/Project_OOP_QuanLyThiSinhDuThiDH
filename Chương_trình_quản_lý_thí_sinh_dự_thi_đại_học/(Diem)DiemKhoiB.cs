5215dde0b3c94654c31f6b786e51803b2928a56f
nnt7733 <thoain.n2006@gmail.com>
2025-10-02 15:31:03 +0700

    Refactor mon descriptor handling

--- Chương_trình_quản_lý_thí_sinh_dự_thi_đại_học/(Diem)DiemKhoiB.cs
+++ Chương_trình_quản_lý_thí_sinh_dự_thi_đại_học/(Diem)DiemKhoiB.cs
@@ -8,11 +8,17 @@ namespace Chương_trình_quản_lý_thí_sinh_dự_thi_đại_học
         public double Hoa { get; set; }
         public double Sinh { get; set; }
 
-        protected override (string TenMon, Func<double> Getter, Action<double> Setter)[] MonHoc => new[]
+        protected override MonHocDescriptor[] MonHoc
         {
-            ("Toán", () => Toan, value => Toan = value),
-            ("Hóa", () => Hoa, value => Hoa = value),
-            ("Sinh", () => Sinh, value => Sinh = value),
-        };
+            get
+            {
+                return new MonHocDescriptor[]
+                {
+                    new MonHocDescriptor("Toán", () => Toan, value => Toan = value),
+                    new MonHocDescriptor("Hóa", () => Hoa, value => Hoa = value),
+                    new MonHocDescriptor("Sinh", () => Sinh, value => Sinh = value),
+                };
+            }
+        }
     }
 }
